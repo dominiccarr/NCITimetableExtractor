fbdb5ce535795b5fd2a3624c5939ccd79a0a361b
Dominic <dominic@Dominics-Mac.local>
2017-10-23 19:20:57 +0100

    updated this to produce an output of staff based availability

--- TimetableExtractor.rb
+++ TimetableExtractor.rb
@@ -1,6 +1,54 @@
 require 'nokogiri'
 require 'pp'
 require 'cgi'
+require "csv"
+
+DAYS=Hash["Mon", 1, "Tue", 2, "Wed", 3, "Thu", 4, "Fri", 5, "Sat", 6]
+
+class Timeslot
+	attr_accessor :location, :weeks, :start_time, :end_time, :day
+  
+  def initialize(location, weeks, start_time, end_time, day)
+    @location = location
+    @weeks = weeks
+    @start_time = start_time
+    @end_time = end_time
+    @day = day
+  end
+  
+  def to_s
+    "#{weeks} #{start_time}-#{end_time} #{day}"
+  end
+  
+end
+
+class StaffMember
+  
+  attr_accessor :time_slots, :name
+  
+  def initialize(name)
+    @name = name
+    @time_slots = Hash["Mon", [], "Tue", [], "Wed", [], "Thu", [], "Fri", [], "Sat", []]
+
+  end
+  
+  def add_slot(slot)
+    # @time_slots[slot.day] = [] if @time_slots[slot.day] == nil
+    @time_slots[slot.day] << slot    
+    
+    @time_slots[slot.day].sort! { |i,j| i.start_time.to_i <=> j.start_time.to_i }
+  end
+  
+  def to_s
+    str = "#{name}\n"
+    @time_slots.each do |k,v| 
+      str += "#{k}\n"
+      v.each { |s| str += "#{s}\n" }
+    end
+    return str
+  end
+  
+end
 
 class TeachingSession
 	
@@ -19,6 +67,10 @@ class TeachingSession
     @day = day
     filter
 	end
+  
+  def raw_day
+    return @day
+  end
   
   def day 
     days=Hash["Mon", 1, "Tue", 2, "Wed", 3, "Thu", 4, "Fri", 5, "Sat", 6]
@@ -119,33 +171,77 @@ doc.search('/html/body/table[contains(@class, "grid-border-args")]').each do |ce
 end
 
 sessions = sessions.uniq
-require "csv"
 
-max = sessions.max_by { |mod| mod.programmes.length }
-max_progs = max.programmes.length
-max = sessions.max_by { |mod| mod.team.length }
-max_team = max.team.length
-max = sessions.max_by { |mod| mod.assistants.length }
-max_assistants = max.assistants.length
+def create_csv_timetable(sessions)
+  max = sessions.max_by { |mod| mod.programmes.length }
+  max_progs = max.programmes.length
+  max = sessions.max_by { |mod| mod.team.length }
+  max_team = max.team.length
+  max = sessions.max_by { |mod| mod.assistants.length }
+  max_assistants = max.assistants.length
 
-CSV.open("NCI Timetable.csv", "wb") do |csv|
-  headers = %w(Module Type) 
-  max_team.times { |i| headers << "Team Member #{i+1}" }
-  
-  max_assistants.times { |i| headers << "Assistant #{i+1}" }
+  CSV.open("NCI Timetable.csv", "wb") do |csv|
+    headers = %w(Module Type) 
+    max_team.times { |i| headers << "Team Member #{i+1}" }
 
-  headers << %w(Location Weeks Day Start End)
-  max_progs.times { |i| headers << "Programme #{i+1}" }
-  headers.flatten!
-  
-  csv << headers
-  
-  sessions.each { |session| 
-    arr = [session.module_name, session.type]
-    max_team.times { |i| arr << session.team[i] }
-    max_assistants.times { |i| arr << session.assistants[i] }
-    arr += [session.location, session.weeks, session.day, session.start_time, session.end_time]
-    max_progs.times { |i| arr << session.programmes[i] }
-    csv << arr
-  }
+    max_assistants.times { |i| headers << "Assistant #{i+1}" }
+
+    headers << %w(Location Weeks Day Start End)
+    max_progs.times { |i| headers << "Programme #{i+1}" }
+    headers.flatten!
+
+    csv << headers
+
+    sessions.each { |session| 
+      arr = [session.module_name, session.type]
+      max_team.times { |i| arr << session.team[i] }
+      max_assistants.times { |i| arr << session.assistants[i] }
+      arr += [session.location, session.weeks, session.day, session.start_time, session.end_time]
+      max_progs.times { |i| arr << session.programmes[i] }
+      csv << arr
+    }
+  end
 end
+
+def create_lecturer_view(sessions)
+  lecturers = []
+  sessions.each { |session| lecturers << session.team }
+  lecturers.flatten!.uniq!
+  lecturers.map! { |l| StaffMember.new(l) }
+  map = {}
+  lecturers.each { |l| map[l.name] = l }
+  
+  sessions.each do |session|
+    map.each do |name, obj|
+      if session.team.include? name        
+        obj.add_slot Timeslot.new(session.location, session.weeks, session.start_time, session.end_time, session.raw_day)
+      end
+    end
+  end
+  return map
+end
+
+def create_lecturer_csv(map)
+  CSV.open("Lecturer Availability.csv", "wb") do |csv|
+    headers = %w(Lecturer Mon Tues Wed Thur Fri Sat) 
+    csv << headers
+
+    map.each { |name, obj| 
+      arr = [name]
+      obj.time_slots.each do |k,v|    
+        str = []
+        v.each do |slot|
+          str << "#{slot.start_time}-#{slot.end_time} (#{slot.weeks})"
+        end
+        arr << str.join(",")
+      end
+      csv << arr
+    }
+  end
+  
+end
+
+# create_csv_timetable(sessions)
+map = create_lecturer_view(sessions)
+create_lecturer_csv(map)
+
